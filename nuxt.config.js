5eab1a246e0cc862d9f8705a44c3517181a2c1a0
nicholas <nicholas.us.03@gmail.com>
2024-04-20 22:44:30 +0300

    update source

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,17 +6,17 @@ export default {
       lang: 'en'
     },
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''},
-      {name: 'format-detection', content: 'telephone=no'}
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/icon.ico'}
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }
     ]
   },
-ssr: false,
-target: 'static',
+  ssr: false,
+  target: 'static',
   mode: 'spa',
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/style/index.css'],
